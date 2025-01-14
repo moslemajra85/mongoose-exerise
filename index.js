2ee42bb375789a81092c71d9a8afdeea20165601
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-01-14 20:22:49 +0100

    Add exercise desciption

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+|--------------------------------------------------
+| 1- Get All the published backend courses,
+  sort them by their name,
+  pick only their name and author,
+  and display them
+
+  2 - Get all the published frontend and backend courses,
+  sort them by their price in descending order,
+  pick only  their name and author.
+  display them
+
+  3 - Get All published course that are $15 or more
+  or have the word 'by' in their title
+|--------------------------------------------------
+*/
